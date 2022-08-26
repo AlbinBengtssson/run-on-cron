7ea8739b6428f28a6f21dbb3d7638cbfee7b17ed
Albin Bengtsson <albben97@gmail.com>
2022-08-26 15:30:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Run on cron
+
+This repository aims to convert cron notation to be able to run as sleeps in Python.
+
+NOTE: At the time there is no functionality for handling combinations of operators yet. For example '0,1/5 \* \* \* \*' will not work properly.
+There is also no functionality for '/' (step values) yet.
+
+To run the program run:
+
+'''
+python run_on_cron.py <command to run on loop> <cron string>
+'''
+
+For example:
+'''
+python run_on_cron.py "python test_script.py" "\* \* \* \* \*"
+'''
---
+++ run_on_cron.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+from time import sleep
+from subprocess import Popen
+from sys import argv
+
+# TODO: Add functionality for combined operators such as '0,1/5 * * * *' as well as functionality for '/' (step values).
+
+'''
+Cron Cheat Sheet:
+
+┌───────────── minute (0 - 59)
+│ ┌───────────── hour (0 - 23)
+│ │ ┌───────────── day of the month (1 - 31)
+│ │ │ ┌───────────── month (1 - 12 or JAN-DEC)
+│ │ │ │ ┌───────────── day of the week (0 - 6 or SUN-SAT)
+│ │ │ │ │
+│ │ │ │ │
+│ │ │ │ │
+* * * * *
+
+*   :   Any value
+,   :   Value list separator
+-   :   Range of values
+/   :   Step values
+
+Example:
+'5 0 * 8 *'
+'At 00:05 in August'
+
+'''
+
+
+def cron_time_match(cron_val, time_val):
+    # Checks whether the time_val is within the specification of the cron value.
+    # NOTE: This only checks a single value, not a complete cron string.
+
+    match = False
+
+    if cron_val == '*' or cron_val == time_val:
+        match = True
+    elif '-' in cron_val:
+        cron_val_min = int(cron_val.split('-')[0])
+        cron_val_max = int(cron_val.split('-')[1])
+        if (int(time_val) <= cron_val_max and int(time_val) >= cron_val_min):
+            match = True
+    elif ',' in cron_val:
+        values = cron_val.split(',')
+        if time_val in values:
+            match = True
+
+    elif '/' in cron_val:
+        # Dividable (%), (x/y) -> every y minute from x through max
+
+        raise ValueError("Not yet able to handle step values '/'")
+
+    return match
+
+
+def weekday_to_cron(datetime_weekday):
+    # Converts datetime's (0-6, MON-SUN) to Cron's (0-6, SUN-SAT)
+
+    if datetime_weekday == 6:
+        return 0
+    else:
+        return datetime_weekday + 1
+
+
+def check_time_to_execute(cron):
+    # Checks whether it is time to execute the program according to the cron specification.
+
+    current_time = datetime.now()
+
+    if (cron_time_match(cron[3], str(current_time.month))
+            and cron_time_match(cron[4], str(weekday_to_cron(current_time.weekday())))
+            and cron_time_match(cron[2], str(current_time.day))
+            and cron_time_match(cron[1], str(current_time.hour))
+            and cron_time_match(cron[0], str(current_time.minute))
+            and current_time.second == 0):
+        return True
+
+    return False
+
+
+# Input:
+#       command:    Command that shoudl be ran, for example 'python script.py' or 'bash script.sh'
+#       cron:       Cron expression in the form of a string, for example '5 4 * * *'.
+if __name__ == "__main__":
+    command = argv[1]
+    cron = argv[2].split()
+
+    while True:
+        if check_time_to_execute(cron):
+            Popen(command, shell=True, stdout=False)
+            sleep(50)
+
+        while datetime.now().second != 0:
+            sleep(0.5)
