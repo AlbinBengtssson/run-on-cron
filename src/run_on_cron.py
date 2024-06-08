a70f1193a5a8408b4c17cabcdc6f6f62282a6b4d
Albin Bengtsson <albben97@gmail.com>
2024-06-08 10:09:18 +0200

    Add typing, black, isort, flake8 etc.

--- src/run_on_cron.py
+++ src/run_on_cron.py
@@ -1,77 +1,85 @@
 from datetime import datetime
-from time import sleep
 from subprocess import Popen
 from sys import argv
+from time import sleep
+from typing import List, Tuple
 
-# TODO: Add check for month and day of the week. Now it can only handle input in the form of integers,
+# TODO: Add check for month and day of the week. Now it can only handle input in the form of integers, # noqa: E501
 #       not 'JAN' or 'MON', etc.
 
-'''
-Cron Cheat Sheet:
+"""
+    Cron Cheat Sheet:
 
-┌───────────── minute (0 - 59)
-│ ┌───────────── hour (0 - 23)
-│ │ ┌───────────── day of the month (1 - 31)
-│ │ │ ┌───────────── month (1 - 12 or JAN-DEC)
-│ │ │ │ ┌───────────── day of the week (0 - 6 or SUN-SAT)
-│ │ │ │ │
-│ │ │ │ │
-│ │ │ │ │
-* * * * *
+    ┌───────────── minute (0 - 59)
+    │ ┌───────────── hour (0 - 23)
+    │ │ ┌───────────── day of the month (1 - 31)
+    │ │ │ ┌───────────── month (1 - 12 or JAN-DEC)
+    │ │ │ │ ┌───────────── day of the week (0 - 6 or SUN-SAT)
+    │ │ │ │ │
+    │ │ │ │ │
+    │ │ │ │ │
+    * * * * *
 
-*   :   Any value
-,   :   Value list separator
--   :   Range of values
-/   :   Step values
+    *   :   Any value
+    ,   :   Value list separator
+    -   :   Range of values
+    /   :   Step values
 
-Example:
-'5 0 * 8 *'
-'At 00:05 in August'
+    Example:
+    '5 0 * 8 *'
+    'At 00:05 in August'
 
-'''
+"""
 
 
-def cron_time_match(cron_val, time_val):
-    # Checks whether the time_val is within the specification of the cron value.
-    # NOTE: This only checks a single value, not a complete cron string.
-    # TODO: Handle combined inputs such as '1-5/3,5'.
+def cron_time_match(cron_val: str, time_val: str) -> bool:
+    """
+    Checks whether the time_val is within the specification of the cron value.
+    NOTE: This only checks a single cron value, not a complete cron string.
+    """
 
-    match = False
+    # TODO (Albin): Handle combined inputs such as '1-5/3,5'.
 
-    if ((',' in cron_val and '/' in cron_val)
-        or (',' in cron_val and '-' in cron_val)
-            or ('/' in cron_val and '-' in cron_val)):
+    if (
+        ("," in cron_val and "/" in cron_val)
+        or ("," in cron_val and "-" in cron_val)
+        or ("/" in cron_val and "-" in cron_val)
+    ):
         raise ValueError(
-            "Invalid cron input. Note that the program cannot handle combined inputs such as '1-5/3,50'")
+            "Invalid cron input. The program cannot handle combined inputs such as '1-5/3,50'"  # noqa:E501
+        )
 
-    elif cron_val == '*' or cron_val == time_val:
-        match = True
+    elif cron_val == "*" or cron_val == time_val:
+        return True
 
-    elif '-' in cron_val:
-        cron_val_min = int(cron_val.split('-')[0])
-        cron_val_max = int(cron_val.split('-')[1])
-        if (int(time_val) <= cron_val_max and int(time_val) >= cron_val_min):
-            match = True
+    elif "-" in cron_val:
+        cron_val_min = int(cron_val.split("-")[0])
+        cron_val_max = int(cron_val.split("-")[1])
+        if int(time_val) <= cron_val_max and int(time_val) >= cron_val_min:
+            return True
 
-    elif ',' in cron_val:
-        values = cron_val.split(',')
+    elif "," in cron_val:
+        values = cron_val.split(",")
         if time_val in values:
-            match = True
+            return True
 
-    elif '/' in cron_val:
-        cron_step_vals = cron_val.split('/')
+    elif "/" in cron_val:
+        cron_step_vals = cron_val.split("/")
 
-        if cron_step_vals[0] != '*':
+        if cron_step_vals[0] != "*":
             raise ValueError(
-                "Non standard cron input. Step values must be specified as '*/x' where x is a value.")
+                "Non standard cron input. Step values must be specified as '*/x' where x is a value."  # noqa: E501
+            )
         if int(time_val) % int(cron_step_vals[1]) == 0:
-            match = True
+            return True
 
-    return match
+    return False
 
 
-def weekday_to_cron(datetime_weekday):
-    # Converts datetime's (0-6, MON-SUN) to Cron's (0-6, SUN-SAT)
+def weekday_to_cron(datetime_weekday: int) -> int:
+    """
+    Converts datetime's (0-6, MON-SUN) to Cron's (0-6, SUN-SAT)
+    """
 
     if datetime_weekday == 6:
         return 0
@@ -79,59 +87,72 @@ def weekday_to_cron(datetime_weekday):
         return datetime_weekday + 1
 
 
-def check_cron_input(cron):
-    # Makes sure that the cron input is of the right length.
-    # TODO: Make more robust. Check if the values are valid.
+def validate_cron_input(cron: str) -> None:
+    """
+    Ensures that the cron input is of the right length and form.
+    """
+    # TODO (Albin): Validate that it is a valid cron string using regex
 
     try:
         assert len(cron.split()) == 5
-    except AssertionError as e:
+    except AssertionError:
         raise AssertionError(
-            "Invalid cron input, must be five values with space inbetween. For example: '* * * * *'.")
+            "Invalid cron input, must be five values with space inbetween. For example: '* * * * *'."  # noqa: E501
+        )
 
 
-def check_time_to_execute(cron, time):
-    # Checks whether it is time to execute the program according to the cron specification.
+def check_time_to_execute(cron: List[str], time: datetime) -> bool:
+    """
+    Checks whether it is time to execute the program according to the cron
+    specification.
+    """
 
-    if (cron_time_match(cron[3], str(time.month))
-            and cron_time_match(cron[4], str(weekday_to_cron(time.weekday())))
-            and cron_time_match(cron[2], str(time.day))
-            and cron_time_match(cron[1], str(time.hour))
-            and cron_time_match(cron[0], str(time.minute))
-            and time.second == 0):
-
+    if (
+        cron_time_match(cron[3], str(time.month))
+        and cron_time_match(cron[4], str(weekday_to_cron(time.weekday())))
+        and cron_time_match(cron[2], str(time.day))
+        and cron_time_match(cron[1], str(time.hour))
+        and cron_time_match(cron[0], str(time.minute))
+        and time.second == 0
+    ):
         return True
 
     return False
 
 
-def filter_input(args):
-    command = ''
-    cron = ''
+def filter_input(args: List[str]) -> Tuple[str, str]:
+    command = ""
+    cron = ""
 
     for arg in args:
-        if '--cron=' in arg:
-            cron = arg.split('=')[1]
-        elif '--command=' in arg or '--cmd=' in arg:
-            command = arg.split('=')[1]
+        if "--cron=" in arg:
+            cron = arg.split("=")[1]
+        elif "--command=" in arg or "--cmd=" in arg:
+            command = arg.split("=")[1]
     return command, cron
 
 
 # Input:
-#       command:    Command that shoudl be ran, for example 'python script.py' or 'bash script.sh'
+#       command:    Command that shoudl be ran, for example 'python script.py' or 'bash script.sh' # noqa: E501
 #       cron:       Cron expression in the form of a string, for example '5 4 * * *'.
 if __name__ == "__main__":
     command, cron = filter_input(argv)
 
-    check_cron_input(cron)
+    validate_cron_input(cron)
 
-    print('run_on_cron launched at:', datetime.now(),
-          '\nwith command:', command, '\nand cron expression:', cron)
+    print(
+        "run_on_cron launched at:",
+        datetime.now(),
+        "\nwith command:",
+        command,
+        "\nand cron expression:",
+        cron,
+    )
 
     while True:
 
         if check_time_to_execute(cron.split(), datetime.now()):
-            print('Executing:', command, 'at:', datetime.now())
+            print("Executing:", command, "at:", datetime.now())
             Popen(command, shell=True)
             sleep(50)
 
--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,13 @@
 import datetime
+
 from pytest import raises
-from src.run_on_cron import weekday_to_cron, check_time_to_execute, cron_time_match, check_cron_input
+
+from src.run_on_cron import (
+    check_time_to_execute,
+    cron_time_match,
+    validate_cron_input,
+    weekday_to_cron,
+)
 
 
 def test_weekday_to_cron():
@@ -23,127 +30,208 @@ def test_weekday_to_cron():
 def test_cron_time_match():
     # Combined input
     with raises(ValueError) as error_info:
-        cron_time_match('1-5/2', '1')
-    assert str(error_info.value) == "Invalid cron input. Note that the program cannot handle combined inputs such as '1-5/3,50'"
+        cron_time_match("1-5/2", "1")
+    assert (
+        str(error_info.value)
+        == "Invalid cron input. The program cannot handle combined inputs such as '1-5/3,50'"  # noqa: E501
+    )
 
     # Same value
-    assert cron_time_match('1', '1')
+    assert cron_time_match("1", "1")
 
     # Any value
-    assert cron_time_match('*', '33')
+    assert cron_time_match("*", "33")
 
     # Range of values
     for i in range(1, 10):
-        assert cron_time_match('1-10', str(i))
+        assert cron_time_match("1-10", str(i))
     for i in range(1, 10):
-        assert cron_time_match('11-20', str(i)) == False
+        assert cron_time_match("11-20", str(i)) is False
 
     # Step values
     with raises(ValueError) as error_info:
-        cron_time_match('1/2', '1')
-    assert str(
-        error_info.value) == "Non standard cron input. Step values must be specified as '*/x' where x is a value."
+        cron_time_match("1/2", "1")
+    assert (
+        str(error_info.value)
+        == "Non standard cron input. Step values must be specified as '*/x' where x is a value."  # noqa: E501
+    )
 
-    assert cron_time_match('*/2', '24')
-    assert cron_time_match('*/2', '3') == False
+    assert cron_time_match("*/2", "24")
+    assert cron_time_match("*/2", "3") is False
 
     # Value list separator
-    assert cron_time_match('1-15', '1')
-    assert cron_time_match('1-15', '15')
-    assert cron_time_match('1-15', '4')
-    assert cron_time_match('1-15', '0') == False
-    assert cron_time_match('1-15', '16') == False
+    assert cron_time_match("1-15", "1")
+    assert cron_time_match("1-15", "15")
+    assert cron_time_match("1-15", "4")
+    assert cron_time_match("1-15", "0") is False
+    assert cron_time_match("1-15", "16") is False
 
 
 def test_check_time_to_execute():
 
     # Every minute
     assert check_time_to_execute(
-        ['*', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 23, 0))
-    assert check_time_to_execute(
-        ['*', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 23, 1)) == False
+        ["*", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 23, 0)
+    )
+    assert (
+        check_time_to_execute(
+            ["*", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 23, 1)
+        )
+        is False
+    )
 
     # Every hour
     assert check_time_to_execute(
-        ['0', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 0, 0))
-    assert check_time_to_execute(
-        ['0', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 23, 0)) == False
+        ["0", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 0, 0)
+    )
+    assert (
+        check_time_to_execute(
+            ["0", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 23, 0)
+        )
+        is False
+    )
 
     # Every day
     assert check_time_to_execute(
-        ['0', '0', '*', '*', '*'], datetime.datetime(2022, 8, 29, 0, 0, 0))
-    assert check_time_to_execute(
-        ['0', '0', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 23, 0)) == False
+        ["0", "0", "*", "*", "*"], datetime.datetime(2022, 8, 29, 0, 0, 0)
+    )
+    assert (
+        check_time_to_execute(
+            ["0", "0", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 23, 0)
+        )
+        is False
+    )
 
     # Every month
     assert check_time_to_execute(
-        ['0', '0', '1', '*', '*'], datetime.datetime(2022, 9, 1, 0, 0, 0))
-    assert check_time_to_execute(
-        ['0', '0', '1', '*', '*'], datetime.datetime(2022, 8, 29, 11, 23, 0)) == False
+        ["0", "0", "1", "*", "*"], datetime.datetime(2022, 9, 1, 0, 0, 0)
+    )
+    assert (
+        check_time_to_execute(
+            ["0", "0", "1", "*", "*"], datetime.datetime(2022, 8, 29, 11, 23, 0)
+        )
+        is False
+    )
 
     # Every year
     assert check_time_to_execute(
-        ['0', '0', '1', '1', '*'], datetime.datetime(2022, 1, 1, 0, 0, 0))
-    assert check_time_to_execute(
-        ['0', '0', '1', '1', '*'], datetime.datetime(2022, 8, 29, 11, 23, 0)) == False
+        ["0", "0", "1", "1", "*"], datetime.datetime(2022, 1, 1, 0, 0, 0)
+    )
+    assert (
+        check_time_to_execute(
+            ["0", "0", "1", "1", "*"], datetime.datetime(2022, 8, 29, 11, 23, 0)
+        )
+        is False
+    )
 
     # Range of values, minutes 1-12
     assert check_time_to_execute(
-        ['1-12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 1, 0))
+        ["1-12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 1, 0)
+    )
     assert check_time_to_execute(
-        ['1-12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 4, 0))
+        ["1-12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 4, 0)
+    )
     assert check_time_to_execute(
-        ['1-12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 12, 0))
-    assert check_time_to_execute(
-        ['1-12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 0, 0)) == False
-    assert check_time_to_execute(
-        ['1-12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 13, 0)) == False
+        ["1-12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 12, 0)
+    )
+    assert (
+        check_time_to_execute(
+            ["1-12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 0, 0)
+        )
+        is False
+    )
+    assert (
+        check_time_to_execute(
+            ["1-12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 13, 0)
+        )
+        is False
+    )
 
     # Step values
     assert check_time_to_execute(
-        ['1,12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 1, 0))
+        ["1,12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 1, 0)
+    )
     assert check_time_to_execute(
-        ['1,12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 12, 0))
+        ["1,12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 12, 0)
+    )
     assert check_time_to_execute(
-        ['1,12,26', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 26, 0))
-    assert check_time_to_execute(
-        ['1,12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 4, 0)) == False
-    assert check_time_to_execute(
-        ['1,12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 24, 0)) == False
-    assert check_time_to_execute(
-        ['1,12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 13, 0)) == False
+        ["1,12,26", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 26, 0)
+    )
+    assert (
+        check_time_to_execute(
+            ["1,12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 4, 0)
+        )
+        is False
+    )
+    assert (
+        check_time_to_execute(
+            ["1,12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 24, 0)
+        )
+        is False
+    )
+    assert (
+        check_time_to_execute(
+            ["1,12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 13, 0)
+        )
+        is False
+    )
 
     # Value list separator
     assert check_time_to_execute(
-        ['*/12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 0, 0))
+        ["*/12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 0, 0)
+    )
     assert check_time_to_execute(
-        ['*/12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 12, 0))
+        ["*/12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 12, 0)
+    )
     assert check_time_to_execute(
-        ['*/12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 24, 0))
-    assert check_time_to_execute(
-        ['*/12', '*', '*', '*', '*'], datetime.datetime(2022, 8, 29, 11, 13, 0)) == False
+        ["*/12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 24, 0)
+    )
+    assert (
+        check_time_to_execute(
+            ["*/12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 13, 0)
+        )
+        is False
+    )
 
     with raises(ValueError) as error_info:
-        check_time_to_execute(['1/12', '*', '*', '*', '*'],
-                              datetime.datetime(2022, 8, 29, 11, 13, 0))
-    assert str(
-        error_info.value) == "Non standard cron input. Step values must be specified as '*/x' where x is a value."
+        check_time_to_execute(
+            ["1/12", "*", "*", "*", "*"], datetime.datetime(2022, 8, 29, 11, 13, 0)
+        )
+    assert (
+        str(error_info.value)
+        == "Non standard cron input. Step values must be specified as '*/x' where x is a value."  # noqa: E501
+    )
 
 
-def test_check_cron_input():
+def test_validate_cron_input():
     with raises(AssertionError) as error_info:
-        check_cron_input('*****')
-    assert str(
-        error_info.value) == "Invalid cron input, must be five values with space inbetween. For example: '* * * * *'."
+        validate_cron_input("*****")
+    assert (
+        str(error_info.value)
+        == "Invalid cron input, must be five values with space inbetween. For example: '* * * * *'."  # noqa: E501
+    )
 
     with raises(AssertionError) as error_info:
-        check_cron_input('* * * *')
-    assert str(
-        error_info.value) == "Invalid cron input, must be five values with space inbetween. For example: '* * * * *'."
+        validate_cron_input("* * * *")
+    assert (
+        str(error_info.value)
+        == "Invalid cron input, must be five values with space inbetween. For example: '* * * * *'."  # noqa: E501
+    )
 
     with raises(AssertionError) as error_info:
-        check_cron_input('* * * * * *')
-    assert str(
-        error_info.value) == "Invalid cron input, must be five values with space inbetween. For example: '* * * * *'."
+        validate_cron_input("* * * * * *")
+    assert (
+        str(error_info.value)
+        == "Invalid cron input, must be five values with space inbetween. For example: '* * * * *'."  # noqa: E501
+    )
 
-    check_cron_input('* * * * *')
+    validate_cron_input("* * * * *")
+
+    with raises(AssertionError) as error_info:
+        validate_cron_input("* * * * * *")
+    assert (
+        str(error_info.value)
+        == "Invalid cron input, must be five values with space inbetween. For example: '* * * * *'."  # noqa: E501
+    )
+
+    validate_cron_input("* * * * *")
